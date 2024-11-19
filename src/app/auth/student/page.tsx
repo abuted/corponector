14fa03f859204300c48e5423039699defe6c0146
Alexis Karl Buted <akbuted@hawaii.edu>
2024-11-18 22:46:52 -1000

    updated

--- src/app/auth/student/page.tsx
+++ src/app/auth/student/page.tsx
@@ -1,4 +1,5 @@
 // src/app/student/page.tsx
+
 'use client';
 
 import { useForm } from 'react-hook-form';
@@ -16,7 +17,7 @@ const StudentPage = () => {
   const validationSchema = Yup.object().shape({
     name: Yup.string().required('Name is required'),
     skills: Yup.string().required('Skills are required'),
-    professionalPage: Yup.string().url('Professional page must be a valid URL').required('Professional page is required'),
+    professionalPage: Yup.string().url('Must be a valid URL').required('Professional page is required'),
   });
 
   const {
@@ -39,45 +40,27 @@ const StudentPage = () => {
         {/* Name Input */}
         <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            {...register('name')}
-            isInvalid={!!errors.name}
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.name?.message}
-          </Form.Control.Feedback>
+          <Form.Control type="text" {...register('name')} isInvalid={!!errors.name} />
+          <Form.Control.Feedback type="invalid">{errors.name?.message}</Form.Control.Feedback>
         </Form.Group>
 
         {/* Skills Input */}
         <Form.Group className="mb-3">
           <Form.Label>Skills</Form.Label>
-          <Form.Control
-            type="text"
-            {...register('skills')}
-            isInvalid={!!errors.skills}
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.skills?.message}
-          </Form.Control.Feedback>
+          <Form.Control type="text" {...register('skills')} isInvalid={!!errors.skills} />
+          <Form.Control.Feedback type="invalid">{errors.skills?.message}</Form.Control.Feedback>
         </Form.Group>
 
         {/* Professional Page URL Input */}
         <Form.Group className="mb-3">
           <Form.Label>Professional Page</Form.Label>
-          <Form.Control
-            type="url"
-            {...register('professionalPage')}
-            isInvalid={!!errors.professionalPage}
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.professionalPage?.message}
-          </Form.Control.Feedback>
+          <Form.Control type="url" {...register('professionalPage')} isInvalid={!!errors.professionalPage} />
+          <Form.Control.Feedback type="invalid">{errors.professionalPage?.message}</Form.Control.Feedback>
         </Form.Group>
 
         {/* Brings Student to Student Page */}
         <Button type="submit" className="btn btn-primary">
-            <a href="/auth/student">Submit</a>
+          <a href="/auth/student">Submit</a>
         </Button>
       </Form>
     </div>
